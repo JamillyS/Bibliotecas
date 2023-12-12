71cc938476d6d3533b536cf336679c08d56bc7a5
Jamilly da Silva Pinheiro <jamilly.p11@aluno.ifsc.edu.br>
2023-12-12 14:27:48 -0300

    implementação da arvore

--- libprg/src/include/libprg/arvore.h
+++ libprg/src/include/libprg/arvore.h
@@ -4,22 +4,34 @@
 #include "stdio.h"
 #include "stdbool.h"
 #include "libprg.h"
+#include <string.h>
+
+struct Usuario{
+    char login[10];
+    char senha[10];
+    char nome[30];
+};
 
 typedef struct no {
-    int valor;
+    struct Usuario usuario;
+
     struct no *esquerda;
     struct no *direita;
 } no_t;
 
-no_t *criar_no(int valor);
-void destruir_no(no_t *no);
-no_t *inserir_valor(no_t *raiz, int valor);
-bool busca(no_t *raiz, int valor);
+no_t *criar_no(struct Usuario usuario);
+void destruir_no_arvore(no_t *no);
 
-int buca_maior_valor(no_t *raiz);
-void imprimir_maior_valor(no_t *raiz);
-int busca_menor_valor(no_t *raiz);
-void imprimir_menor_valor(no_t *raiz);
+no_t *inserir_valor(no_t *raiz, struct Usuario valor);
+
+bool busca_usuario(no_t *raiz, char *login, char *senha);
+
+struct Usuario busca_maior_usuario(no_t *raiz);
+void imprimir_maior_usuario(no_t *raiz);
+
+struct Usuario busca_menor_usuario(no_t *raiz);
+void imprimir_menor_usuario(no_t *raiz);
+//
 
 
 #endif
--- libprg/src/libprg/arvore.c
+++ libprg/src/libprg/arvore.c
@@ -1,65 +1,82 @@
 #include "libprg/arvore.h"
 
-no_t *criar_no(int valor){
-    no_t *no = (no_t *) malloc(sizeof(no_t));
-    no->valor = valor;
+// Esta função cria um nó e inicializa seus campos
+no_t *criar_no(struct Usuario usuario) {
+    // aloca memória para o nó
+    no_t *no = (no_t *)malloc(sizeof(no_t));
+    no->usuario = usuario;
     no->esquerda = no->direita = NULL;
     return no;
 }
 
-void destruir_no(no_t *no) {
+// Esta função destrói um nó e libera a memória
+void destruir_no_arvore(no_t *no) {
     if (no != NULL) {
-    destruir_no(no->esquerda);
-    destruir_no(no->direita);
-    free(no);
+        destruir_no_arvore(no->esquerda);
+        destruir_no_arvore(no->direita);
+        free(no);
     }
 }
 
 // todo nó é raiz de uma subárvore
-no_t *inserir_valor(no_t *raiz, int valor){
+no_t *inserir_valor(no_t *raiz, struct Usuario novoUsuario) {
+    // se a árvore estiver vazia, retorna um novo nó
     if (raiz == NULL) {
-        return criar_no(valor);
-    } else if (valor < raiz->valor) {
-        raiz->esquerda = inserir_valor(raiz->esquerda, valor);
-    } else if (valor > raiz->valor) {
-        raiz->direita = inserir_valor(raiz->direita, valor);
+        return criar_no(novoUsuario);
+        // se não, percorre a árvore e adiciona o nó na posição correta
+    } else if (strcmp(novoUsuario.login, raiz->usuario.login) < 0) {
+        raiz->esquerda = inserir_valor(raiz->esquerda, novoUsuario);
+        // se não, percorre a árvore e adiciona o nó na posição correta
+    } else if (strcmp(novoUsuario.login, raiz->usuario.login) > 0) {
+        raiz->direita = inserir_valor(raiz->direita, novoUsuario);
     }
     return raiz;
 }
 
-bool busca(no_t *raiz, int valor){
+// Esta função busca o usuário na árvore
+bool busca_usuario(no_t *raiz, char *login, char *senha) {
     if (raiz == NULL) {
-        printf("valor %d não encontrado\n", valor);
+        printf("Usuário ou Senha inválidos %s \n", raiz->usuario.login, login);
         return false;
     }
-    if (valor == raiz->valor){
-        printf("valor %d encontrado\n", valor);
+    if (strcmp(raiz->usuario.login, login) == 0 && strcmp(raiz->usuario.senha, senha) == 0) {
+        printf("Usuário encontrado. Bem vindo %s !\n", raiz->usuario.nome);
         return true;
     }
-    if (valor < raiz->valor) return busca(raiz->esquerda, valor);
-    return busca(raiz->direita, valor);
+
+    // se não, percorre a árvore e adiciona o nó na posição correta
+    if (strcmp(login, raiz->usuario.login) < 0) {
+        return busca_usuario(raiz->esquerda, login, senha);
+    } else {
+        return busca_usuario(raiz->direita, login, senha);
+    }
 }
 
-int buca_maior_valor(no_t *raiz) {
+// Esta função busca o maior usuário na árvore
+struct Usuario busca_maior_usuario(no_t *raiz) {
     while (raiz->direita != NULL) {
         raiz = raiz->direita;
     }
-    return raiz->valor;
+    return raiz->usuario;
 }
 
-void imprimir_maior_valor(no_t *raiz) {
-    int maior = buca_maior_valor(raiz);
-    printf("O maior valor é %d\n", maior);
+// Esta função imprime o maior usuário na árvore
+void imprimir_maior_usuario(no_t *raiz) {
+    struct Usuario maior = busca_maior_usuario(raiz);
+    printf("O maior login: %s. Com o nome: %s, é o maior usuário da árvore\n", maior.login, maior.nome);
 }
 
-int busca_menor_valor(no_t *raiz) {
+//Esta função busca o menor usuário na árvore
+struct Usuario busca_menor_usuario(no_t *raiz) {
     while (raiz->esquerda != NULL) {
         raiz = raiz->esquerda;
     }
-    return raiz->valor;
+    return raiz->usuario;
 }
 
-void imprimir_menor_valor(no_t *raiz) {
-    int menor = busca_menor_valor(raiz);
-    printf("O menor valor é %d\n", menor);
+// Esta função imprime o menor usuário na árvore
+void imprimir_menor_usuario(no_t *raiz) {
+    struct Usuario menor = busca_menor_usuario(raiz);
+    printf("O maior login: %s. Com o nome: %s, é o menor usuário da ávore\n", menor.login, menor.nome);
 }
+
